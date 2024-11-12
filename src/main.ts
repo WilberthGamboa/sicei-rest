74f9affe9a7a7ddcbc1952a4f4c0482005d3863d
Wilberth Gamboa <wmgc3060@gmail.com>
2024-11-12 16:49:01 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,12 @@ async function bootstrap() {
       },
     }),
   );
+  app.enableCors({
+    origin: '*',  // Permitir todos los orígenes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Métodos permitidos
+    allowedHeaders: 'Content-Type, Accept, Authorization',  // Cabeceras permitidas
+    credentials: true,  // Permitir el uso de cookies y credenciales
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
